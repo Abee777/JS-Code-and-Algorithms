5b690541449d85aa0db8f81381c2af7ed416b31c
Vladimir Salatic <vlasalatic@gmail.com>
2019-12-18 15:32:07 +0100

    10. Sort unique elements, 11.

---
+++ Intermediate Algorithm Scripting/11. Convert HTML Entities.js
@@ -0,0 +1,9 @@
+/* Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a 
+string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+//Chaining of replace method with different arguments
+  str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");
+return str;
+}
